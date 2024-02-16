96aa62eb3eed855322b80bf8096d6d28ea13b733
budhaditya-itobuz <it012303@IT012303s-MacBook-Pro.local>
2024-02-16 17:28:57 +0530

    code corrected

--- buttons/index.js
+++ buttons/index.js
@@ -3,16 +3,16 @@ let count = 0;
 const render = () => {
     count++;
     console.log(count);
-    if (count % 4 == 0) {
+    if (count % 4 === 0) {
         const button = document.createElement("button");
         button.innerText = "Click Me";
         button.setAttribute("onclick", "render()");
         document.getElementsByClassName("container")[0].appendChild(button);
     }
-    var allchar = "0123456789ABCDEF";
+    const hexSet = "0123456789ABCDEF";
     color = "";
     for (var i = 0; i < 6; i++) {
-        color += allchar[Math.floor(Math.random() * 16)];
+        color += hexSet[Math.floor(Math.random() * 16)];
     }
     console.log(color);
     document.getElementsByTagName("body")[0].style.backgroundColor = "#" + color;
--- color-change/index.js
+++ color-change/index.js
@@ -2,5 +2,4 @@ const color = (e) => {
     styles = getComputedStyle(e)
     styles.backgroundColor
     document.getElementsByTagName('body')[0].style.backgroundColor = styles.backgroundColor
-
 }
